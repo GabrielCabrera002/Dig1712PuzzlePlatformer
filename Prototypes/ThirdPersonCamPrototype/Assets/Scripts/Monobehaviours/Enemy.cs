c10924c2adf9a64c37ddc11adf2eb4213e476423
AlexanderVillarreal <villarrealaw@gmail.com>
2017-11-27 20:19:54 -0500

    Added an enemy that follows player

---
+++ Prototypes/ThirdPersonCamPrototype/Assets/Scripts/Monobehaviours/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject player;
+	NavMeshAgent agent;
+	Vector3 enemyPosition;
+	Vector3 playerPosition;
+	Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DetectionDistance ();
+
+		agent.SetDestination (player.transform.position);
+	}
+
+	void DetectionDistance() {
+		enemyPosition = this.gameObject.transform.position;
+		playerPosition = player.transform.position;
+		direction = playerPosition - enemyPosition;
+
+		Debug.DrawRay (transform.position, direction, Color.green);
+	}
+}
